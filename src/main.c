42ef8282a8e65caef9d450d2c0ea78f87fa5f0b7
LuccaC.M <git.yfm@slmail.me>
2023-03-13 07:49:25 -0400

    Added right/left player y variables

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ typedef struct {
 // Global Variables
 int screen_width, screen_height;
 Ball ball;
+int right_player_y, left_player_y;
 // Function Declarations
 void DrawGraphics();
 
@@ -21,6 +22,8 @@ int main (void) {
     screen_height = GetScreenHeight();
     ball.posX = screen_width / 2;
     ball.posY = screen_height / 2;
+    left_player_y = (screen_height - 50) / 2;
+    right_player_y = (screen_height - 50) / 2;
     while (!WindowShouldClose()) {
         DrawGraphics();
     }
@@ -32,9 +35,9 @@ void DrawGraphics() {
     BeginDrawing();
         ClearBackground(BLACK);
         // Left Player
-        DrawRectangle(screen_width / 10, (screen_height - 50) / 2, 15, 100, WHITE);
+        DrawRectangle(screen_width / 10, left_player_y, 15, 100, WHITE);
         // Right Player
-        DrawRectangle(screen_width * 9 / 10, (screen_height - 50) / 2, 15, 100, WHITE);
+        DrawRectangle(screen_width * 9 / 10, right_player_y, 15, 100, WHITE);
         // Ball
         DrawCircle(ball.posX, ball.posY, 5, WHITE);
     EndDrawing();
