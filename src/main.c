92aa38078778196dac4c8f16b3fc851e1d42e65c
LuccaC.M <git.yfm@slmail.me>
2023-03-13 08:05:26 -0400

    Added Player Structs

--- src/main.c
+++ src/main.c
@@ -6,10 +6,17 @@ typedef struct {
     int posX, posY;
 }Ball;
 
+typedef struct {
+    // Cordinates
+    int x, y;
+    // Movement variables
+    int speed, direction;
+}Player;
+
 // Global Variables
 int screen_width, screen_height;
 Ball ball;
-int right_player_y, left_player_y;
+Player right_player, left_player;
 // Function Declarations
 void DrawGraphics();
 
@@ -20,24 +27,23 @@ int main (void) {
     SetTargetFPS(60);
     screen_width = GetScreenWidth();
     screen_height = GetScreenHeight();
-    ball.posX = screen_width / 2;
-    ball.posY = screen_height / 2;
-    left_player_y = (screen_height - 50) / 2;
-    right_player_y = (screen_height - 50) / 2;
-    while (!WindowShouldClose()) {
-        DrawGraphics();
-    }
-    
-    return 0;
+//  Ball
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+//  Players
+    left_player.y = (screen_height - 50) / 2;
+    left_player.x = screen_width / 10;
+    right_player.y = (screen_height - 50) / 2;
+    right_player.x = screen_width * 9 / 10;
 }
 
 void DrawGraphics() {
     BeginDrawing();
         ClearBackground(BLACK);
         // Left Player
-        DrawRectangle(screen_width / 10, left_player_y, 15, 100, WHITE);
+        DrawRectangle(screen_width / 10, left_player.y, 15, 100, WHITE);
         // Right Player
-        DrawRectangle(screen_width * 9 / 10, right_player_y, 15, 100, WHITE);
+        DrawRectangle(screen_width * 9 / 10, right_player.x, 15, 100, WHITE);
         // Ball
         DrawCircle(ball.posX, ball.posY, 5, WHITE);
     EndDrawing();
